cedcb95b76a0a0ebf7f8345941587e87e01668a5
luis ramirez <luis.ramirez0001@outlook.com>
2023-06-01 17:03:03 -0500

    ejercicio 2 promedio de notas

--- taller 2/02-taller.js
+++ taller 2/02-taller.js
@@ -1,1 +1,14 @@
 //Ingresar tres calificaciones, calcular el promedio y determinar si el alumno aprobó la materia (3.5 es la nota //mínima para pasar).
+
+
+let  Nota1= Number(prompt('Digita nota 1 '));
+let  Nota2= Number(prompt('Digita nota 2 '));
+let  Nota3= Number(prompt('Digita nota 3 '));
+
+let promedio = (Nota1 + Nota2 + Nota3) / 3;
+if(promedio <= 3.4 ){
+    console.log('NO aprobaste');
+}
+else if(promedio >= 3.5){
+    console.log('Aprobaste');
+}
